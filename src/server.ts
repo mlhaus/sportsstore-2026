10d2033e67f5144d17ee489acdc21215d2176e5c
Marc Hauschildt <marc.hauschildt@gmail.com>
2026-08-25 15:12:05 -0500

    Setting up server configuration, Aug 25

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import { createServer } from "http";
+import express, { Express } from "express";
+import helmet from "helmet";
+import { getConfig } from "./config";
+const port = getConfig("http:port", 5000);
+const expressApp: Express = express();
+expressApp.use(helmet());
+expressApp.use(express.json());
+expressApp.use(express.urlencoded({extended: true}))
+expressApp.get("/", (req, resp) => {
+    resp.send("Hello, SportsStore");
+})
+const server = createServer(expressApp);
+server.listen(port,
+    () => console.log(`HTTP Server listening on port ${port}`));
