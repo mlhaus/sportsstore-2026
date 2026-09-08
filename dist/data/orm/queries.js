7d4af2f89b3292763d054eea2c0a0793e1d8cdc6
Marc Hauschildt <marc.hauschildt@gmail.com>
2026-09-08 15:01:42 -0500

    Added filtering parameters, Sep 8

--- dist/data/orm/queries.js
+++ dist/data/orm/queries.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AddQueries = AddQueries;
 const models_1 = require("./models");
+const sequelize_1 = require("sequelize");
 function AddQueries(Base) {
     return class extends Base {
         async getProducts(params) {
@@ -9,6 +10,17 @@ function AddQueries(Base) {
             if (params?.page && params.pageSize) {
                 opts.limit = params?.pageSize,
                     opts.offset = (params.page - 1) * params.pageSize;
+            }
+            if (params?.searchTerm) {
+                const searchOp = { [sequelize_1.Op.like]: "%" + params.searchTerm + "%" };
+                opts.where = {
+                    [sequelize_1.Op.or]: { name: searchOp, description: searchOp }
+                };
+            }
+            if (params?.category) {
+                opts.where = {
+                    ...opts.where, categoryId: params.category
+                };
             }
             const result = await models_1.ProductModel.findAndCountAll({
                 include: [
@@ -18,7 +30,8 @@ function AddQueries(Base) {
                 raw: true, nest: true,
                 ...opts
             });
-            return { products: result.rows, totalCount: result.count };
+            const categories = await this.getCategories();
+            return { products: result.rows, totalCount: result.count, categories };
         }
         getCategories() {
             return models_1.CategoryModel.findAll({
--- dist/routes/catalog.js
+++ dist/routes/catalog.js
@@ -6,8 +6,10 @@ const createCatalogRoutes = (app) => {
     app.get("/", async (req, resp) => {
         const page = Number.parseInt(req.query.page?.toString() ?? "1");
         const pageSize = Number.parseInt(req.query.pageSize?.toString() ?? "3");
-        const res = await data_1.catalog_repository.getProducts({ page, pageSize });
-        resp.render("index", { ...res, page, pageSize, pageCount: Math.ceil(res.totalCount / (pageSize ?? 1)) });
+        const searchTerm = req.query.searchTerm?.toString();
+        const category = Number.parseInt(req.query.category?.toString() ?? "");
+        const res = await data_1.catalog_repository.getProducts({ page, pageSize, searchTerm, category });
+        resp.render("index", { ...res, page, pageSize, pageCount: Math.ceil(res.totalCount / (pageSize ?? 1)), searchTerm, category });
     });
     app.get("/err", (req, resp) => {
         throw new Error("Something bad happened");
