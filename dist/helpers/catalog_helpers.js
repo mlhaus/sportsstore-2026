ea91b5928e7eec144e04f6d72f4040e4b55388c8
Marc Hauschildt <marc.hauschildt@gmail.com>
2026-09-08 14:29:12 -0500

    Added pagination links, Sep 8

---
+++ dist/helpers/catalog_helpers.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.pageButtons = exports.escapeUrl = exports.navigationUrl = void 0;
+const querystring_1 = require("querystring");
+const querystring_2 = require("querystring");
+const getData = (options) => {
+    return { ...options.data.root, ...options.hash };
+};
+const navigationUrl = (options) => {
+    const { page, pageSize } = getData(options);
+    return "/?" + (0, querystring_1.stringify)({ page, pageSize });
+};
+exports.navigationUrl = navigationUrl;
+const escapeUrl = (url) => (0, querystring_2.escape)(url);
+exports.escapeUrl = escapeUrl;
+const pageButtons = (options) => {
+    const { page, pageCount } = getData(options);
+    let output = "";
+    for (let i = 1; i <= pageCount; i++) {
+        output += options.fn({
+            page, pageCount, index: i, selected: i === page
+        });
+    }
+    return output;
+};
+exports.pageButtons = pageButtons;
